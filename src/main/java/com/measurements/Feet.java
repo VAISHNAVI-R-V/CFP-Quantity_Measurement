7aaf40012cfd421cbac43931131b1ada98394128
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-10-31 20:01:28 +0530

    ADDED: Welcome Messsage.

---
+++ src/main/java/com/measurements/Feet.java
@@ -0,0 +1,13 @@
+package com.measurements;
+/**
+ * Purpose : To implement Quantity Measurement Program.
+ *
+ * @author VAISHNAVI VISHWAKARMA
+ * @since 31-10-2021
+ */
+public class Feet {
+    // Method to print welcome message
+    public void message() {
+        System.out.println("Welcome to Quantity Measurement Program");
+    }
+}
---
+++ src/test/java/com/measurements/QuantityMeasurementTest.java
@@ -0,0 +1,11 @@
+package com.measurements;
+
+import org.junit.jupiter.api.Test;
+
+public class QuantityMeasurementTest {
+    @Test
+    void welcome_Message() {
+        Feet feet = new Feet();
+        feet.message();
+    }
+}
