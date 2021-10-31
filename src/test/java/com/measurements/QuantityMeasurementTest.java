56eb5cd1f05dd25cb5e3b5dafba3444eea7cb59f
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-10-31 20:59:22 +0530

    ADDED: Test case to perform Equality for TYPE Check.

--- src/test/java/com/measurements/QuantityMeasurementTest.java
+++ src/test/java/com/measurements/QuantityMeasurementTest.java
@@ -24,4 +24,11 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(1.0);
         Assertions.assertNotEquals(feet1, feet2);
     }
+
+    @Test
+    void given_Type0FeetAnd1Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assertions.assertEquals(feet1.getClass(), feet2.getClass());
+    }
 }
